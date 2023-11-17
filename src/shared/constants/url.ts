2b052df31556e01e09bc0ef90f0ccfb9f2036ba4
Jean Danilo Maxwell Patrick Pootaren <danilo.pootaren@mns.mu>
2023-11-17 14:34:33 +0400

    Remove boilerplate code

---
+++ src/shared/constants/url.ts
@@ -0,0 +1,2 @@
+// @Local
+export const BASE_URL = 'http://localhost:8050';
---
+++ src/shared/utils/colors.ts
@@ -0,0 +1,14 @@
+export const ColorPalette = {
+  primary: '#273892',
+  secondary: '#268FDC',
+  white: 'white',
+  boldText: '#505354',
+  paleGreyText: '#747779',
+  disabledButton: '#C9C9C9',
+  black: 'black',
+  paleborder: 'rgba(0, 0, 0, 0.10)',
+  background: '#F3F4F9',
+  overlay: '#00000040',
+  text: '#F4F4F4',
+  inputDisabled: '#F2F4F9',
+};
